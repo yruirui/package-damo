824a455c7f99cc29001c6ae183335a555df3c998
ruirui <yruirui96@gmail.com>
2021-06-22 23:45:43 +0800

    第二次提交

--- dist/index.bd78eeec.js
+++ dist/index.bd78eeec.js
@@ -452,10 +452,60 @@ dom.after(child, div1);
 const div2=dom.create('<span>往前添加</span>');
 dom.before(child,div2);
 
-const div3=dom.create("<span>添加child</span>");
+const div3=dom.create("<div id='1'></div>");
+dom.before(dog,div3);
 
-const div5=dom.create("<span>22</span>");
-dom.wrap(a,div5)
+const div5=dom.create("<span></span>");
+ dom.wrap(div3,dog);
+
+ dom.remove(remove);
+
+ const nodes=dom.empty(window.empty);
+ console.log(nodes);
+
+ dom.attr(add,'title','hi,i am yr');
+ const title =dom.attr(add,'title');
+ console.log(`title:${title}`);
+
+ dom.text(text1,'这是新内容');
+
+ //dom.html(add2, '<span>hi</span>');
+
+ dom.style(style, {border: '1px solid red', color: 'blue'})
+ console.log(dom.style(style, 'border'))
+ dom.style(style, 'border', '1px solid black')
+
+dom.class.add(text6,'red')
+dom.class.add(text6,'blue')
+dom.class.remove(text6,'blue')
+console.log(dom.class.has(text6,'red'))
+
+const fn=()=>{
+    console.log('点击了')
+}
+dom.on(ye,'click',fn)
+dom.off( ye, 'click', fn)
+
+  const testDiv=dom.find('#travel')[0]
+  console.log(testDiv)
+  const test8=dom.find('#t1',)[0]
+  console.log(test8)
+
+  const s2=dom.find('#s2')[0]
+  console.log(dom.sibling(s2))
+  console.log(dom.next(s2))
+  console.log(dom.previous(s2))
+
+  const t= dom.find('#travel')[0]
+  dom.each(dom.child(t),(n)=>dom.style(n,'color','red'))
+  
+
+  console.log(dom.index(s2))
+//   const t = dom.find('#travel')[0]
+//   dom.each(dom.children(t), (n)=> dom.style(n, 'color', 'red'))
+  
+//   console.log(dom.index(s2))
+
 
 },{}]},["1Ypau","30Yv7"], "30Yv7", "parcelRequire427e")
 
--- src/dom.js
+++ src/dom.js
@@ -16,8 +16,124 @@ window.dom={
     wrap(parent,node){
         dom.before(node,parent);
         dom.append(parent,node);
-        //先给a添加一个哥哥，然后把哥哥变成爸爸
-        dom.before(node, parent)
+    },
+    remove(node){
+        node.parentNode.removeChild(node);
+        return node;
+    },
+    empty(node){
+        let arr=[];
+        let  x=node.firstChild;
+        while(x){
+          arr.push(dom.remove(node.firstChild));
+          x=node.firstChild;
+        }
+        return arr;
+    },
+    attr(node,name,vale){//重载
+        if(arguments.length===3){
+            node.setAttribute(name,vale);
+        }else if(arguments.length=2){
+            return node.getAttribute(name);
+        }
+    },
+    text(node,string){//适配
+        if(arguments.length===2){
+            if( 'innerTEXT' in node){
+                node.innerTEXT=string;
+            }else{
+                node.textContent=string;
+            } 
+        if(arguments.length===1){
+            if( 'innerTEXT' in node){
+                return node.innerTEXT;
+            }else if(textContent in node){
+                return node.textContent;
+            }
+        }
+       }
+    },
+    html(node,string){
+        if(arguments.length=== 2){
+            node.innerHTML=string;}
+        else if(arguments.length === 1){
+            return node.innerHTML;
+        }   
+    },
+    style(node,name,vale){
+        if(arguments.length===3){
+            node.style[name]=vale;
+        }else if(arguments.length===2){
+            if(typeof name==='string'){
+                //dom.style(div,'color')
+                return node.style[name];
+            }else if(name instanceof Object){
+                let object=name;
+                for(key in object){
+                    node.style[key]=object[key];
+                }
+            }
+        }
+    },
+    class:{
+        add(node,className){
+            node.classList.add(className);
+        },
+        remove(node,className){
+            node.classList.remove(className);
+        },
+        has(node,className){
+            return node.classList.contains(className);
+        }
+    },
+    on(node,eventName,fn){
+        node.addEventListener(eventName,fn)
+    },
+    off(node,eventName,fn){
+        node.removeEventListener(eventName,fn)
+    },
+    find(selector,scope){
+        return (scope || document).querySelectorAll(selector)        
+    },
+    parent(node){
+        return node.parentNode
+    },
+    child(node){
+        return node.children
+    },
+    sibling(node){
+        return Array.from(node.parentNode.children)
+        .filter(n=>n!==node)//filter过滤
+    },
+    next(node){
+        let x=node.nextSibling
+        while(x&&x.nodeType===3){
+            x=x.nextSibling;
+        }
+        return x;
+    },
+    previous(node){
+        let x=node.previousSibling;
+        if(x&&x.nodeType===3){
+            x=node.previousSibling
+        }
+        return x;
+    },
+    each(nodeList,fn){
+        for(let i=0;i<nodeList.length;i++){
+            fn.call(null,nodeList[i])
+        }
+
+    },
+    index(node){
+        const list=dom.child(node.parentNode)
+        let i=0
+        for(i=0;i<list.length;i++){
+            if(list[i]===node){
+                break;
+            }
+        }
+        return i;
     }
 
 
